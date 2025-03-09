666639357a2f881d385550d17c62c536af138a2e
Pathum Rathnayaka <thilinapathumrathnayaka@gmail.com>
2025-03-10 00:35:00 +0530

    add album path

--- src/routes/albums.route.js
+++ src/routes/albums.route.js
@@ -1,8 +1,10 @@
 import express from "express";
+import { getAlbumById, getAllAlbums } from "../controller/album.controller.js";
 
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.send("albums route with GET method");
-});
+
+router.get("/", getAllAlbums);
+router.get("/:albumId", getAlbumById);
+
 
 export default router;
